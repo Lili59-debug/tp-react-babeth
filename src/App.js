903228d7994c374f4e0c824e27f3548031488d94
Aurélie Constant <aurelie.constant@outlook.com>
2020-10-29 17:29:06 +0100

    'premieres-routes'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Header from './Components/header/Header'
+import Home from './Components/home/Home'
+import Admin from './Components/admin/Admin'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path="/admin">
+            <Admin />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/admin/Admin.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Admin extends Component {
+    render() {
+        return (
+            <h1>Page ajout de produits</h1>
+        )
+    }
+}
+
+export default Admin
